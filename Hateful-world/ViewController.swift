dec116101c07de087aaeaac7980a2233f926138a
Iain276 <iain276@gmail.com>
2016-08-29 21:43:04 +0100

    Initial Commit

---
+++ Hateful-world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hateful-world
+//
+//  Created by Iain Mitchell on 29/08/2016.
+//  Copyright © 2016 Iain Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
